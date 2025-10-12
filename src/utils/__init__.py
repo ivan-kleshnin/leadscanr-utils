69f51b2b0f7dd551859ea854ebf6fe2f94982464
Ivan Kleshnin <ivan@paqmind.com>
2025-10-12 21:26:58 +0300

    Expose some off-the-shelf `process` function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,13 @@
+from .bucket import bucket_numbers
+from .cleanup import clean_text
+from .fold import fold_currencies, fold_numbers, fold_scale_units
+from .mask import mask_contacts
 
+def process(text: str) -> str:
+    text = mask_contacts(text)
+    text = fold_currencies(text)
+    text = fold_numbers(text)
+    text = fold_scale_units(text)
+    text = clean_text(text)
+    text = bucket_numbers(text)
+    return text
